21a41c1d699a5fb17b88c5df99051b8fc90909a7
anada <anada@>
2018-04-14 14:42:01 +0900

    test.javaコメント付け。フィボナッチ関数修正

--- javaSorce/test.java
+++ javaSorce/test.java
@@ -1,15 +1,18 @@
 class Test{
    private static void main(String hoge[]){
+       //変数を定義
        int[] list = new int[]{1,2,3,4,5};
        int   fa   = 6 ;
        int   fi   = 8 ;
-
+　　　　
+       //各関数を実行
        System.out.println("sum:" + sum(list));
        System.out.println("ave:" + ave(list));
        System.out.println("fac:" + factory(fa));
        System.out.println("fib:" + fibonacci(fi));
    }
-
+   
+   //リスト内の合計値を出す
    private static int sum(int[] list){
         int sum = 0;
         for(int i = 0; i < list.length; i++){
@@ -18,10 +21,12 @@ class Test{
         return sum;
    }
 
+   //リスト内の数値の平均値
    private static int ave(int[] list){
         return sum(list)/list.length;
    }
 
+   //与えられた値ｎの階乗
    private static int factory(int hoge){
         if(hoge > 0){
             return hoge * factory(hoge - 1);
@@ -30,10 +35,11 @@ class Test{
         }
     }
 
+    //ｎ番目のフィボナッチ
    private static int fibonacci(int fibo){
         if(fibo == 1){
             return 1;
-        }else if(fibo == 0){
+        }else if(fibo =< 0){
             return 0;
         }else{
             return fibonacci(fibo -2) + fibonacci(fibo -1);
