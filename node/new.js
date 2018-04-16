1d2aa652d99e9d6c84643774cac35e3d335c56d0
anada <anada@>
2018-04-16 21:31:56 +0900

    modulestest

--- node/new.js
+++ node/new.js
@@ -1,11 +1,11 @@
 const http = require('http');
-      fs   = require('fs');
-      setting = fs.readFile('./setting.js','utf-8');
+      fs   = require('setting');
 
 const server = http.createServer();
 server.on('request', function(req, res){
     res.writeHead(200, { 'Content-Type': 'text/plain' }),
     res.write('helloworld!!'),
     res.end()
-    }).listen();
-console.log('server running : port 1234');
+    }).listen(setting.port);
+    
+console.log('server running : ' + setting.console + ":" + setting.port);
