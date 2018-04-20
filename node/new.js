0cb421e24f7f9753e0120eda59960c31211befc4
anada <anada@pc>
2018-04-20 23:00:01 +0900

    new.jschaged

--- node/new.js
+++ node/new.js
@@ -1,23 +1,11 @@
 const http = require('http');
-<<<<<<< HEAD
-fs = require('setting');
-=======
       setting   = require('./setting.js');
->>>>>>> 1bb9b98c6b3d0a9762e8966534b4fe7cfa258cf4
 
 const server = http.createServer();
 server.on('request', function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' }),
-<<<<<<< HEAD
-        res.write('helloworld!!'),
-        res.end()
-}).listen(setting.port);
-
-console.log('server running : ' + setting.console + ":" + setting.port);
-=======
     res.write('hello world!!'),
     res.end()
     }).listen(setting.port);
     
 console.log('server running : ' + setting.console + ":" + setting.port);
->>>>>>> 1bb9b98c6b3d0a9762e8966534b4fe7cfa258cf4
