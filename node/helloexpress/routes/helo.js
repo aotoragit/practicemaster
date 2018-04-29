4778f4de4cfa1b888e2f1498f7866f9459d1fba5
anada <anada@pc>
2018-04-29 12:26:57 +0900

    bbs making

--- node/helloexpress/routes/helo.js
+++ node/helloexpress/routes/helo.js
@@ -1,16 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var msglist = [];
+var fs = require('fs');
+
 
 /*get helo page */
 router.get('/',function(req, res, next){
-    res.render('helo',{
-        title: 'helo',
-        data: {
-            'taro':'taro@yamada',
-            'hanako':'hanako@flower',
-            'tuyano':'syoda@tuyano.com'
-        }
+    var p1 = req.query.p1;
+    var p2 = req.query.p2;
+    var msg = p1 ===undefined ? "no query" : p1 +"," + p2;
+    res.render('helo',
+    {
+        title: 'helo page',
+        msg  : msg,
+        input: '',
+        msglist:''
     });
 });
+
+router.post('/',function(req,res,next){
+    var str = req.body['input1'];
+    if(str !== ""){
+        msglist.push(str);
+    }
+
+    res.render('helo',
+        {
+            title:'helo page',
+            msg: "your typed :" + str,
+            input: '',
+            msglist: msglist
+        }
+    )
+    fs.appendFile("bbs.txt", str + ",", (err) =>{
+        if(err){
+            console.log('err')
+        }else{
+            console.log('succes')
+        }
+    })   
+})
 
 module.exports = router;
